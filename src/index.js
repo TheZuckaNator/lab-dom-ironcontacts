e7936892d1e9130a5ea222c144af4c0400700bb3
AMGI <amgi@mac.home>
2025-05-14 11:14:39 +0200

    works with out bonus

--- src/index.js
+++ src/index.js
@@ -21,12 +21,12 @@ exampleRow.innerHTML = `
   <td> ${randomContact.name} </td>
   <td> ${randomContact.popularity.toFixed(2)} </td>
   <td>
-    <button class="btn-delete">Delete</button>
-  </td>
-  <td>
     <button class="btn-like">
       <img src="./images/icon.png" alt="like" />
     </button>
+  </td>
+    <td>
+    <button class="btn-delete">Delete</button>
   </td>
 `;
 
@@ -39,26 +39,60 @@ tableBody.appendChild(exampleRow);
 const threeContacts = contacts.splice(0, 3);
 
 // Step 2: Loop through each contact
-// Step 3: Create a row for each contact
-// Step 4: Set the row's HTML
-// Step 5: Append the row to the table
+threeContacts.forEach((contact) =>{
+  // Step 3: Create a row for each contact
+  const newRow = document.createElement('tr');
+  // Step 4: Set the row's HTML
+  newRow.innerHTML = `
+    <td>
+      <img src="${contact.pictureUrl}" />
+    </td>
+    <td>${contact.name}</td>
+    <td>${contact.popularity.toFixed(2)}</td>
+    <td>
+      <button class="btn-like">
+        <img src="./images/icon.png" alt="like" />
+      </button>
+    </td>
+    <td>
+      <button class="btn-delete">Delete</button>
+    </td>
+  `;
+  
+  // Step 5: Append the row to the table
+  tableBody.appendChild(newRow);
 
 
 // ITERATION 2 - Delete Buttons
 // Step 1: Get all delete buttons
-// Step 2: Add click event listener to each button
-// Step 3: When clicked, remove the parent row
+  const deleteButton = newRow.querySelector(".btn-delete");
+  // Step 2: Add click event listener to each button
+  deleteButton.addEventListener("click", () => {
+    // Step 3: When clicked, remove the parent row
+
+    newRow.remove();
+  })
 
 
 // ITERATION 3 - Like Buttons
 // Step 1: Get all like buttons
+const likeButton = newRow.querySelector(".btn-like");
 // Step 2: Add click event listener to each button
-// Step 3: When clicked, toggle the 'selected' class
+likeButton.addEventListener("click", () => {
+
+  // Step 3: When clicked, toggle the 'selected' class
+  likeButton.classList.toggle("selected");
+});
+});
+
 
 
 // Bonus: ITERATION 4 - Add Random Contacts
 // Step 1: Add click event listener to the add random button
-// Step 2: When clicked, get a random contact
+// buttonAddRandom.addEventListener("click", () => {
+  // Step 2: When clicked, get a random contact
+//   const randomIn
+// })
 // Step 3: Create a new row for the contact
 // Step 4: Add event listeners to the new row's buttons
 // Step 5: Append the row to the table
